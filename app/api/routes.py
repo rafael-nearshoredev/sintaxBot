4e424589999d1bde763444e0363da708ab78960d
Rafael Campo <rafael@nearshoredev.com>
2025-03-17 09:24:15 -0500

    Fix routes

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
-from app.services.syntax_analyzer import analyze_sentence
+from app.services.analyzers.syntax import analyze_sentence
 
 router = APIRouter()
 
